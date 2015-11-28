52b01a83af44eb541c0207f9b96cb0a1d6b7926e
Mohamed Elkebir <elkebir.med@gmail.com>
2015-11-28 23:43:42 +0100

    Test: adding tests to transform(options.date|dateFrom|dateTo)

--- test/index.js
+++ test/index.js
@@ -31,6 +31,15 @@ describe('ARLI', function() {
 
       arli.transform('0123456789').should.be.equal('٠١٢٣٤٥٦٧٨٩');
       arli.transform('0123456789', {digit: false}).should.be.equal('0123456789');
+
+      arli.transform('12/05/1989 12,05,1989 12.05.1989 12-05-1989 12-05-89').should.be.equal('١٢؍٠٥؍١٩٨٩ ١٢؍٠٥؍١٩٨٩ ١٢؍٠٥؍١٩٨٩ ١٢؍٠٥؍١٩٨٩ ١٢؍٠٥؍٨٩');
+      arli.transform('05/12/1989 05,12,1989 05.12.1989 05-12-1989 05-12-89').should.be.equal('٠٥؍١٢؍١٩٨٩ ٠٥؍١٢؍١٩٨٩ ٠٥؍١٢؍١٩٨٩ ٠٥؍١٢؍١٩٨٩ ٠٥؍١٢؍٨٩');
+      arli.transform('12/05,1989 05-12.1989').should.be.equal('١٢/٠٥,١٩٨٩ ٠٥-١٢.١٩٨٩');
+      arli.transform('12/05/1989 05-12-1989', {digit: false}).should.be.equal('12/05/1989 05-12-1989');
+      arli.transform('27/10/1985 10/27/1985 27-10-85', {dateFrom: 'DMY'}).should.be.equal('٢٧؍١٠؍١٩٨٥ ١٠/٢٧/١٩٨٥ ٢٧؍١٠؍٨٥');
+      arli.transform('27/10/1985 10/27/1985 27-10-85', {dateFrom: 'MDY'}).should.be.equal('٢٧/١٠/١٩٨٥ ١٠؍٢٧؍١٩٨٥ ٢٧-١٠-٨٥');
+      arli.transform('27/10/1985 10/27/1985 27-10-85', {dateTo: 'DMY'}).should.be.equal('٢٧؍١٠؍١٩٨٥ ٢٧؍١٠؍١٩٨٥ ٢٧؍١٠؍٨٥');
+      arli.transform('27/10/1985 10/27/1985 27-10-85', {dateTo: 'MDY'}).should.be.equal('١٠؍٢٧؍١٩٨٥ ١٠؍٢٧؍١٩٨٥ ١٠؍٢٧؍٨٥');
     });
   });
 });
