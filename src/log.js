e003113ce43a115613d30d5958d24b7da78a6f2e
Mohamed Elkebir <elkebir.med@gmail.com>
2015-12-14 23:59:26 +0100

    cleaning

--- src/log.js
+++
@@ -1,8 +0,0 @@
-  function log(value, options) {
-
-    if (_isString(value)) {
-      var logs = [];
-
-      return value;
-    }
-  }
--- src/wrapper/end.js
+++
@@ -1,6 +0,0 @@
-  return {
-    log: log,
-    transform: transform,
-    _version: '0.2.0'
-  };
-}));
